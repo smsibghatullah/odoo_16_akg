c604077deba196363d4338565e9c50b9628f2295
mubeen1020 <m.mubeen1020@gmail.com>
2024-09-11 06:38:35 +0500

    working

--- env-odoo16/lib/python3.10/site-packages/nextcloud/api/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model import Item
-from .properties import Property
--- env-odoo16/lib/python3.10/site-packages/nextcloud/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Models are classes inheriting of nextcloud.api.Model
-"""
-from ..api_wrappers.webdav import File
-from ..api_wrappers.systemtags import Tag
